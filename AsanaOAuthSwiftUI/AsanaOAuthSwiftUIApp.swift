0f44b9f3f153ee2ad84ca01bf994ffc2b35e7d03
Andreas Gjaerum <andreas@burnttoast.no>
2020-11-17 20:04:05 +0100

    Initial Commit

---
+++ AsanaOAuthSwiftUI/AsanaOAuthSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsanaOAuthSwiftUIApp.swift
+//  AsanaOAuthSwiftUI
+//
+//  Created by Andreas Gjaerum on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct AsanaOAuthSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsanaOAuthSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsanaOAuthSwiftUI
+//
+//  Created by Andreas Gjaerum on 17/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
